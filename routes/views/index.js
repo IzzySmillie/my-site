977dae3a25fa241c4383d34afdd849b850ecb084
Isabelle Smillie <isabelle.smillie@gmail.com>
2018-01-10 13:29:44 +0000

    Adding homepage data model

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,10 +1,23 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	// Render the view
-	view.render('index');
+  var Portfolio = keystone.list('Portfolio').model.find({});
+  var Image = Image = keystone.list('Image').model.find({});
+  var entirePortfolio = {
+    portfolio: Portfolio.exec.bind(Portfolio),
+    image: Image.exec.bind(Image),
+  };
+
+  async.parallel(entirePortfolio, function(error, contentImages) {
+    if (error) {
+      res.status(500).send(error);
+      return;
+    }
+    res.render('index', {contentImages: contentImages});
+  })
 };
